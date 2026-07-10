cb4265f9deaaf53c30e153a6d3f917aae38feb4a
Aljoša Dmitrović <aljosadmitrovic44@gmail.com>
2026-07-10 23:28:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/blackjack-barber-novi-sad/",
+  base: './',
   server: {
     host: "::",
     port: 8080,
